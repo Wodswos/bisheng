50e94957c7e7f77a85ec5f0212591299c80140a9
zehua <fivezehua@qq.com>
2022-12-12 14:51:45 +0800

    version 0.0

--- publish/githubpagesWorkflow.py
+++ publish/githubpagesWorkflow.py
@@ -2,10 +2,22 @@ import os
 import sys
 import yaml
 
-# 粗糙的引入方式，至少 work
+# 野蛮的引入方式，至少 work
 sys.path.append(os.path.abspath('./'))
 from common.indexing import get_all_markdown_file
-print(sys.path)
+# print(sys.path)
+
+
+def removing_dir(to_delete_path):
+    if not os.path.exists(to_delete_path):
+        print('要删除的目录 ' + to_delete_path + ' 不存在')
+        return
+    if os.path.isdir(to_delete_path):
+        for item in os.listdir(to_delete_path):
+            removing_dir(to_delete_path + '/' + item)
+    else:
+        os.remove(to_delete_path)
+    print('目录 ' + to_delete_path + ' 已经删除')
 
 
 def markdown_moving(source_root_path, dist_root_path):
@@ -43,25 +55,31 @@ def generate_configuration(dist_folder):
 def generate_action_workflow(dist_repository, workflow_name='suibianxie', template_file_path=None):
     if template_file_path:
         with open(template_file_path) as f:
-            workflow_configuration = yaml.load(f.readlines(template_file_path))
+            workflow_configuration = yaml.load(f, yaml.FullLoader)
     else:
         workflow_configuration = {}
-        
-    # workflow_configuration['name'] = 'suibian xiexie la'
-    # workflow_configuration['on'] = {}
-    # workflow_configuration['on']['push'] = {}
-    # workflow_configuration['on']['push']['branch'] = ["main"]
-    # # workflow_configuration['on']['workflow_dispatch']
+        # workflow_configuration['name'] = 'suibian xiexie la'
+        # workflow_configuration['on'] = {}
+        # workflow_configuration['on']['push'] = {}
+        # workflow_configuration['on']['push']['branch'] = ["main"]
+        # workflow_configuration['on']['workflow_dispatch']
+        # workflow_configuration['permission'] = {
+        #     'content':'read',
+        #     'pages':'write',
+        #     'id-tokent':'write'
+        # }
 
-    # workflow_configuration['permission'] = {
-    #     'content':'read',
-    #     'pages':'write',
-    #     'id-tokent':'write'
-    # }
+        # workflow_configuration['jobs'] = {}
+        # workflow_configuration['jobs']['build'] = {}
+        # workflow_configuration['jobs']['deploy'] = {}
+            
+    # 因为python会把yaml里的 on 读取为 True，故在此修复
+    workflow_configuration['on'] = workflow_configuration[True]
+    workflow_configuration.pop(True)
 
-    # workflow_configuration['jobs'] = {}
-    # workflow_configuration['jobs']['build'] = {}
-    # workflow_configuration['jobs']['deploy'] = {}
+    # list directory
+    workflow_configuration['jobs']['build']['steps'].insert(1, {'name':'list directory', 'run':'ls -lR'})
+
 
 
     # 写入 configuration yaml
@@ -82,29 +100,27 @@ if __name__=='__main__':
     dist_root_path = 'C:/Users/Five/Documents/Github'
     
     repository_name = 'wodswos.github.io'
+    repository_path = dist_root_path + '/' +repository_name
     user_name = repository_name.split('.')[0]
     
     # 一系列会用到的 git 命令，默认已配置 ssh
     git_clone_command = 'git clone git@github.com:' + user_name + '/' + repository_name
     git_add_command = 'git add --all'
-    git_commit_command = 'git commit -m update'
+    git_commit_command = "git commit -m 'update'"
     git_push_command = 'git push git@github.com:'  + user_name + '/' + repository_name
 
-    # 切换工作目录并 clone 仓库
-    # os.chdir(dist_root_dir)
-    # os.system(git_clone_command)
-    repository_path = dist_root_path + '/' +repository_name
+    # 仓库已经存在则切换到目录，否则先 clone 仓库
+    if not os.path.exists(repository_path):
+        os.chdir(dist_root_path)
+        os.system(git_clone_command)
     os.chdir(repository_path)
 
-    # 清空目录，从零重新生成
-    # rebuild_flag = True
-    # if rebuild_flag == True:
-    #     for item in os.listdir():
-    #         if not item.startswith('.'):
-    #             if os.path.isdir(repository_path + '/' + item):
-    #                 os.removedirs(repository_path + '/' + item)
-    #             else:
-    #                 os.remove(repository_path + '/' + item)
+    # 清空除 .git 以外的文件夹，从零重新生成
+    rebuild_flag = True
+    for item in os.listdir(repository_path):
+        if item != '.git' and rebuild_flag == True:
+            print(item)
+            removing_dir(repository_path + '/' + item)
 
 
     # 主逻辑
@@ -113,12 +129,19 @@ if __name__=='__main__':
     markdown_moving(source_file_folder, repository_path)
 
     generate_configuration(repository_path)
-    generate_action_workflow(repository_path, workflow_name='my_workflow', template_file_path='C:/Users/Five/Documents/projects/Pythons/bisheng/resouce/workflow_template/jekyll-gh-pages.yml')
+    generate_action_workflow(
+        repository_path, 
+        workflow_name='my_workflow', 
+        template_file_path='C:/Users/Five/Documents/projects/Pythons/bisheng/resouce/workflow_template/jekyll-gh-pages.yml'
+    )
 
     # 提交，收尾
-    # os.system(git_add_command)
-    # os.system(git_commit_command)
-    # os.system(git_push_command)
+    os.system(git_add_command)
+    print('Added to index .')
+    os.system(git_commit_command)
+    print('Committed to local repository')
+    os.system(git_push_command)
+    print('Pushed to Github')
 
 
 
---
+++ test/workflow.py
@@ -0,0 +1,17 @@
+import yaml
+import os
+
+# template_file_path='C:/Users/Five/Documents/projects/Pythons/bisheng/resouce/workflow_template/jekyll-gh-pages.yml'
+# with open(template_file_path) as f:
+#     # print(f.readlines())
+#     data = yaml.load(f, yaml.FullLoader)
+#     print(data)
+
+repository_name = 'wodswos.github.io'
+user_name = repository_name.split('.')[0]
+git_clone_command = 'git clone git@github.com:' + user_name + '/' + repository_name
+
+
+os.system('git --version')
+os.chdir('C:/Users/Five/Desktop/')
+os.system(git_clone_command)
